dfaa0f6b3c3cb6cb8cb7451ea0f73dc0831f18aa
benboydens <r0857165@student.vives.be>
2022-09-24 02:42:06 +0200

    small fix

--- backend/src/database/populate.js
+++ backend/src/database/populate.js
@@ -14,11 +14,11 @@ try {
 
     // getting collection bookings
     const database = client.db("meeting-room");
-    const bookings_col = database.collection("bookings");
+    const bookings = database.collection("bookings");
 
     // this option prevents additional documents from being inserted if one fails
     const options = { ordered: true };
-    const result = await bookings_col.insertMany(all_bookings, options);
+    const result = await bookings.insertMany(all_bookings, options);
     console.log(`${result.insertedCount} bookings were inserted`);
 
 
@@ -26,13 +26,13 @@ try {
     const data2 = fs.readFileSync('../data/rooms.json', 'utf-8');
 
     // parsing to JSON object
-    const all_rooms = JSON.parse(data);
+    const all_rooms = JSON.parse(data2);
 
     // getting collection bookings
-    const collection = database.collection("rooms");
+    const rooms = database.collection("rooms");
 
     // this option prevents additional documents from being inserted if one fails
-    const result2 = await collection.insertMany(all_rooms, options);
+    const result2 = await rooms.insertMany(all_rooms, options);
     console.log(`${result2.insertedCount} rooms were inserted`);
 
 } catch (exception) {
